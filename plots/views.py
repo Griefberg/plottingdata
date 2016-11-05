0e1de65ddf419044f7fb5c2250726f83d8d27ed1
Роман Соловьев <roman@wheely.com>
2016-11-05 17:04:59 +0300

    update

--- plots/views.py
+++ plots/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the plots index.")
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+# def contact(request):
+#     return render(request, 'forms.html', {})
--- plottingdata/views.py
+++ plottingdata/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
