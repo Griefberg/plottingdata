01b0a2e8c90b66df10719dae5b11e60cd58d7634
Роман Соловьев <roman@wheely.com>
2016-11-05 18:19:15 +0300

    update

--- plottingdata/urls.py
+++ plottingdata/urls.py
@@ -19,11 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from . import views
 
-
-urlpatterns = (
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^plots/', include('plots.urls')),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^about/$', views.about, name='about')
-)
+]
